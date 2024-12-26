d0c85a2a4eb80f372573785ec83743b15749ff02
RjCoder <ej84@njit.edu>
2024-12-26 15:50:10 -0500

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const navigation = [
   },
   {
     name: "Orders",
-    href: "/order",
+    href: "/orders",
   },
   {
     name: "Cart",
@@ -65,11 +65,19 @@ const Navbar = () => {
                 </button>
                 {/* Show dropdowns */}
                 {isDropdownOpen && (
-                  <div>
-                    <ul>
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
                       {navigation.map((item) => (
-                        <li key={item.name}>
-                          <Link to={item.href}>{item.name}</Link>
+                        <li
+                          key={item.name}
+                          onClick={() => setIsDropdownOpen(false)}
+                        >
+                          <Link
+                            to={item.href}
+                            className="block px-4 py-2 text-sm hover:bg-gray-100"
+                          >
+                            {item.name}
+                          </Link>
                         </li>
                       ))}
                     </ul>
